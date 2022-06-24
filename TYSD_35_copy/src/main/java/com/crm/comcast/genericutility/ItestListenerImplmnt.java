054182a58713655570eb9b4c26dbc5b4778fb4c4
Admin <Admin@Admin-PC>
2022-06-24 13:19:20 +0530

    commit-01-added extent report again

--- TYSD_35_copy/src/main/java/com/crm/comcast/genericutility/ItestListenerImplmnt.java
+++ TYSD_35_copy/src/main/java/com/crm/comcast/genericutility/ItestListenerImplmnt.java
@@ -15,52 +15,60 @@ import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
-public class ItestListenerImplmnt implements ITestListener {
 
+public class ItestListenerImplmnt implements ITestListener {
+	
 	ExtentReports report;
 	ExtentTest test;
 
 	public void onTestStart(ITestResult result) {
+
 		test=report.createTest(result.getMethod().getMethodName());
+		
 	}
 
 	public void onTestSuccess(ITestResult result) {
+		
 		test.log(Status.PASS, result.getMethod().getMethodName());
 		test.log(Status.PASS, result.getThrowable());
 		
 	}
-	/**
-	 * To take screenshot of failed test scripts
-	 */
+
 	public void onTestFailure(ITestResult result) {
+		
 		test.log(Status.FAIL,result.getMethod().getMethodName());
 		test.log(Status.FAIL, result.getThrowable());
 		
 	    try {
-	    	String screenShotName;
-				screenShotName = WebDriverUtility.takeScreenShot(BaseClass.sdriver, result.getMethod().getMethodName());
-	    	 test.addScreenCaptureFromPath(screenShotName);
-	    	} catch (Throwable e) 
-			{
+	    	String screenShotName=WebDriverUtility.takeScreenShot(BaseClass.sdriver, result.getMethod().getMethodName());
+	    	test.addScreenCaptureFromPath(screenShotName);
+	    	} catch (Throwable e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    }
+		
 		
 //		String testName = result.getMethod().getMethodName();
-//		System.out.println(testName+"====Executing=====");
+//		System.out.println(testName+"====Executing====");
 //		
-//		EventFiringWebDriver edriver=new EventFiringWebDriver(BaseClass.sdriver);
-//		File src = edriver.getScreenshotAs(OutputType.FILE);
+//		EventFiringWebDriver edrDriver = new EventFiringWebDriver(BaseClass.sdriver);
+//		File src = edrDriver.getScreenshotAs(OutputType.FILE);
+//		
 //		try {
-//			FileUtils.copyFile(src, new File("./screenShot/"+testName+".PNG"));
-//		} catch (Exception e) {
+//			FileUtils.copyFile(src, new File("./screenshots/"+testName+".PNG"));
+//		}
+//		catch(Exception e)
+//		{
 //			e.printStackTrace();
 //		}
-	
+	}
 
 	public void onTestSkipped(ITestResult result) {
+
+
 		test.log(Status.SKIP, result.getMethod().getMethodName());
 		test.log(Status.SKIP, result.getThrowable());
+		
 	}
 
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
@@ -69,23 +77,27 @@ public class ItestListenerImplmnt implements ITestListener {
 	}
 
 	public void onStart(ITestContext context) {
-	ExtentSparkReporter spark=new ExtentSparkReporter("./Extent Reports/report.html");
-	
-	spark.config().setTheme(Theme.DARK);
-	spark.config().setReportName("Framework Extent Report");
-	spark.config().setDocumentTitle("sanjay's document");
-	
-	report=new ExtentReports();
-	report.attachReporter(spark);
-	report.setSystemInfo("createdBy", "Lohith");
-	report.setSystemInfo("ReviwedBy", "Sanjay");
-	report.setSystemInfo("platform", "windows-7");
-	report.setSystemInfo("ServerName","ApacheTomcat");
+		
+		ExtentSparkReporter spark=new ExtentSparkReporter("./TYSD_35_copy/Extent Reports/report.html");
+		
+		spark.config().setTheme(Theme.DARK);
+		spark.config().setReportName("Framework Extent Report");
+		spark.config().setDocumentTitle("sanjay's document");
+		
+		report=new ExtentReports();
+		report.attachReporter(spark);
+		report.setSystemInfo("createdBy", "Lohith");
+		report.setSystemInfo("ReviwedBy", "Deepak");
+		report.setSystemInfo("platform", "windows7");
+		report.setSystemInfo("ServerName","ApacheTomcat");
+		
 	}
 
 	public void onFinish(ITestContext context) {
-		// TODO Auto-generated method stub
+
+
 		report.flush();
+		
 	}
 
 }
